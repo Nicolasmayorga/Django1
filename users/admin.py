8a2e043298b0aa3dc2eb971e7f872b7778be227c
Nicolas Mayorga <nmayorgav@gmail.com>
2020-01-16 18:01:15 -0500

    17. Editaremos el detalle para que sea igual de complejo que el detalle de Usuario y le agregaremos los datos del perfil para no tener que estar cambiando de urls. Usaremos fieldsets y admin.StackedInline.

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,10 @@
 
 #Django
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 #Models
+from django.contrib.auth.models import User
 from users.models import Profile
 
 # Register your models here.
@@ -31,3 +33,45 @@ class ProfileAdmin(admin.ModelAdmin):
         'created',
         'modified',
     )
+
+    fieldsets = (
+        ('Profile', {
+            'fields': (('user', 'picture'),),
+                        
+        }),
+          ('Extra info', {
+            'fields': (
+                ('website', 'phone_number'),
+                ('biography')
+
+            )
+        }),
+
+        ('Metadata', {
+            'fields': (('created', 'modified'),),
+                        
+        }),
+    )
+
+    readonly_fields = ('created', 'modified', 'user')
+
+
+class ProfileInline(admin.StackedInline):
+
+        model = Profile
+        can_delete = False
+        verbose_name_plural = 'profiles'
+
+class UserAdmin(BaseUserAdmin):
+    
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
